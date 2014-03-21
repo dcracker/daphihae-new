ff8ac639a70de1be0b68ca69d6a0f40cb237df68
dcracker <dcracker002@gmail.com>
2014-03-21 18:40:23 +0900

    adjust(elaboration?) codes for XCode include, build configs... before libpng added

---
+++ MultiPlatformFramework/IOS/logger.h
@@ -0,0 +1,14 @@
+//
+//  logger.h
+//  Project-iOS
+//
+//  Created by Robin Kang on 2014. 3. 20..
+//  Copyright (c) 2014년 KangRobin. All rights reserved.
+//
+
+#ifndef Project_iOS_logger_h
+#define Project_iOS_logger_h
+
+
+
+#endif
---
+++ MultiPlatformFramework/IOS/stdafx.h
@@ -0,0 +1,14 @@
+//
+//  stdafx.h
+//  Project-iOS
+//
+//  Created by Robin Kang on 2014. 3. 21..
+//  Copyright (c) 2014년 KangRobin. All rights reserved.
+//
+
+#ifndef Project_iOS_stdafx_h
+#define Project_iOS_stdafx_h
+
+
+
+#endif
